1234f2ccdd321c16307d80c982d3593838b2562b
deep <deep@deeps-MacBook.local>
2016-02-04 21:14:31 -0800

    completed class 12 pair assignment

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repoView.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/repoView.js
@@ -9,10 +9,10 @@
     $about.show().siblings().hide();
   };
 
-  // TODO: How do you want to render a single repo as html? Return your filled in HTML template.
+  // DONE: How do you want to render a single repo as html? Return your filled in HTML template.
   var render = function(repo) {
-
-  };
+    return '<li><a href="' + repo.html_url + '">' + repo.name + '</a></li>'
+};
 
   // DONE: If all the data is loaded, we can prep the UI and render the repos.
   repoView.index = function() {
@@ -21,7 +21,7 @@
     // The jQuery `append` method lets us append an entire array of HTML elements at once,
     // So we can use a little FP to transform our data-set into DOM nodes:
     $('#about ul').append(
-      repos.with('forks_count').map(render)
+      repos.all.map(render)
     );
   };
 
